6313858ff455d6ea9d9ae447f98d3fd3fd09ee7e
joanita-51 <nakityojoanita51@gmail.com>
2022-07-09 16:02:32 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Button, Footer, Header, Navbar, Sidebar, UserProfile} from './components';
+import {Dashboard, Landing, Login, Logout, Notifications, Profile, Results, SignUp} from './pages'
 
 function App() {
   return (
-    <div className="App">
-      <p className='text-3xl font-bold'>Welcome to Quizote</p>
+    <div>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+
+        {/* Dashboard pages */}
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/results' element={<Results/>}/>
+        <Route path='/notifications' element={<Notifications/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/logout' element={<Logout/>}/>
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export {default as Dashboard} from './Dashboard';
+export {default as Landing} from './Landing';
+export {default as Login} from './Login';
+export {default as Logout} from './Logout';
+export {default as Notifications} from './Notifications';
+export {default as Profile} from './Profile';
+export {default as Results} from './Results';
+export {default as SignUp} from './SignUp';
