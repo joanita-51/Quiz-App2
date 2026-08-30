c040b5a0bd1967924cd0a325b5d0a29984d18ffc
joanita-51 <nakityojoanita51@gmail.com>
2026-08-30 13:49:41 +0900

    Remove obsolete Tailwind output

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'hero-pattern': "url('/home/anita/Desktop/Ablestate Work Space/QuizApp/quiz/src/data/img/background1.jpg')",
-
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
